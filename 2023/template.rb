8cf032056ebfc9b21989916f3fd6bad98123ff27
Brian Palmer <brian@codekitchen.net>
2023-12-01 19:37:42 -0700

    2023 day 1 and templte

---
+++ 2023/template.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+###
+# impl
+###
+
+def part1(input)
+end
+
+def part2(input)
+end
+
+###
+# data and runner
+###
+
+SHORT = %{}
+FULL = DATA.read
+
+puts "part1 short", (part1 SHORT)
+puts "part1 full", (part1 FULL)
+puts "part2 short", (part2 SHORT)
+puts "part2 full", (part2 FULL)
+
+__END__
