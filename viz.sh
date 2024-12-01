419075ce27f86880c9f5b432ec1c576704f016ad
Brian Palmer <brian@codekitchen.net>
2024-12-01 15:45:10 -0700

    viz comment

--- viz.sh
+++ viz.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# https://www.reddit.com/r/adventofcode/comments/105iu5u/on_crafting_animated_visualizations/
+
 # ffmpeg -r 30 -i viz/frame-%04d.png video.mp4
 
 /Users/brianp/programming/advent-of-code/2023/16/16.rb example  -v
